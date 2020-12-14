fc24e80a956cc17ed4243d687593737b66a021f0
Seamus <d.seamus.ryan@outlook.com>
2020-12-14 18:12:10 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('build'));
+
+
+const PORT = process.env.PORT || 5000;
+
+// serve main app
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.sendStatus(404));
+
+// global error handler
+app.use((err, req, res, next) => {
+	const defaultErr = {
+		log: 'Express error handler caught unknown middleware error',
+		status: 400,
+		message: { err: 'An error occurred' },
+	};
+	const errorObj = { ...defaultErr, ...err };
+	console.log(errorObj.log);
+	return res.status(errorObj.status).json(errorObj.message);
+});
+
+// starting server
+app.listen(PORT, (err) => {
+  if (err) return console.log(err);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  // entry
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  // module rules
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|gif|jpg)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+  ,
+  // plugins
+  plugins : [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    port: 8080,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+      }
+    }
+  }
+}
