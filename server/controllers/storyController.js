16b2fb0b63e19f6a7a4b0a5135d2cdd693cb18fe
Seamus <d.seamus.ryan@outlook.com>
2020-12-15 14:11:12 -0500

    commiting storyController

---
+++ server/controllers/storyController.js
@@ -0,0 +1,2 @@
+const db  = require('../')
+const query = require()
---
+++ server/routes/router.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const Controller = require('../controllers/storyController.js')
+
+router.get('/', Controller. , (req, res) => {
+  
+})
+
+router.post('/', Controller. , (req, res) => {
+  
+})
+
+router.delete('/', Controller. , (req, res) => {
+  
+})
