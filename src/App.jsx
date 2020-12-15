bba0bd093198567a2d4870b984f476baafa49ff9
Kim Mai Nguyen <kim.mai.e.nguyen@gmail.com>
2020-12-14 17:14:38 -0700

    set up the frontend data of redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import  { useSelector, useDispatch } from 'react-redux';
+import { IncreaseCounter } from './redux/actions/actions';
+
+
+
+
+
 export default function App() {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const increaseCounter = () => dispatch(IncreaseCounter());
+
+  const handleClick = () => {
+    increaseCounter(); 
+  };
+
+  
 
   return (
     <div>
       <h1>Hello </h1>
+      <button onClick={handleClick}>hello: {counter}</button>
     </div>
   )
 }
---
+++ src/redux/actions/actions.js
@@ -0,0 +1,5 @@
+import * as types from '../actionsTypes/actionsTypes.js';
+
+export const IncreaseCounter = () => ({
+	type: types.INCREASE,
+});
